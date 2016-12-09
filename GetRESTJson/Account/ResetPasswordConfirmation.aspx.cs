8a41b4b270861e11c601ac95b3054d247ea82950
cnsdwang@gmail.com <cnsdwang@gmail.com>
2016-12-09 11:22:52 -0500

    Add project files.

---
+++ GetRESTJson/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GetRESTJson.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GetRESTJson/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetRESTJson.Startup))]
+namespace GetRESTJson
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
